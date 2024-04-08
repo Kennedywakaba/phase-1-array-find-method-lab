9b95c9daa6b79789000cd4d11ed87915045988a2
kennedy <kennedywakaba5@gmail.com>
2024-04-08 21:38:12 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function superbowlWin(records) {
+    const winningRecord = records.find(record => record.result === "W");
+    return winningRecord ? winningRecord.year : undefined;
+}
+
+const records = [
+    { year: 2000, result: "L" },
+    { year: 2001, result: "W" },
+    { year: 2002, result: "L" },
+    { year: 2003, result: "W" }
+];
+
+
